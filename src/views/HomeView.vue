9ff6826c9ef06e728a98fb3821859bf908aedc78
Neel <njoshi4433@gmail.com>
2024-05-10 18:43:41 +0530

    create dropdown in home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
+  
   <ion-app>
+    
     <ion-header>
       <ion-toolbar color="primary">
         <ion-title>Vue Capacitor</ion-title>
       </ion-toolbar>
     </ion-header>
 
+      
     <ion-content padding>
       <p>The world is your oyster.</p>
       <p>
@@ -14,6 +17,21 @@
         Neel.
       </p>
 
+      <!-- <ion-list style="width: 30%;"> -->
+      <!-- <ion-item class="custom-item-1"> -->
+        <ion-select
+        interface="popover"
+        class="custom-select-box"
+        placeholder="Order By"
+        >
+        <ion-select-option value="apples">Apples</ion-select-option>
+        <ion-select-option value="oranges">Oranges</ion-select-option>
+        <ion-select-option value="bananas">Bananas</ion-select-option>
+          
+        </ion-select>
+      <!-- </ion-item> -->
+    <!-- </ion-list> -->
+
       <!-- Button to navigate to the About page -->
       <ion-button @click="goToAbout" full>Go to About</ion-button>
 
@@ -21,13 +39,18 @@
       <ion-button id="present-alert" @click="showDialogAlert" full
         >Show Alert Box</ion-button
       >
+      <!-- aria-label="Fruit" -->
+      
     </ion-content>
+
+    
   </ion-app>
 </template>
 
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
 import { Dialog } from "@capacitor/dialog";
+import { IonList, IonItem, IonSelect, IonSelectOption, IonPopover } from "@ionic/vue";
 
 // Initializing router
 const router = useRouter();
@@ -46,4 +69,29 @@ const showDialogAlert = async () => {
 };
 </script>
 
-<style scoped></style>
+<style>
+
+ion-popover [popover]:not(:popover-open):not(dialog[open]) {
+    display: contents;
+}
+/* Add any custom styles for ion-select */
+/* For example, to reduce the dropdown size */
+.ion-select-popover {
+  max-width: 200px; /* Adjust the width as needed */
+}
+
+.popover-content{
+  bottom: 12px;
+    top: 182px !important;
+    left: 12px;
+    transform-origin: left top;
+    transform: scale(1);
+    max-height: max-content !important;
+}
+
+.custom-select-box{
+  border: 1px solid black;
+  border-radius: 8px;
+  width: 15%;
+}
+</style>
